51ee0f15e2989be2b7862f13b342324605a3c595
lssalgado <lucas.salgado@uniriotec.br>
2018-08-07 08:33:59 -0300

    All functionalities implemented as main.

--- getter.py
+++ getter.py
@@ -1,5 +1,24 @@
+#coding: utf-8
+
 import urllib, json
 url = "https://raw.githubusercontent.com/muxidev/desafio-android/master/fruits.json"
 response = urllib.urlopen(url)
 data = json.loads(response.read())
-print data
+# print data
+
+aux = 0
+dolarToReal = 3.5
+
+print len(data["fruits"])
+
+print("Selecione a fruta desejada:")
+
+for i in data["fruits"]:
+    print str(aux) + " - " + i["name"] + ": " + str(i["price"])
+    aux = aux + 1
+
+fruit = input("")
+
+selectedFruit = data["fruits"][fruit]
+
+print selectedFruit["name"] + "\nPrice:  $" + str(selectedFruit["price"]) + "\nPreco: R$" + str(selectedFruit["price"]*3.5)
