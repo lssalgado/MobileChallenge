8f7bdd4f60542e675aa94e8e1a2ca442aec73cbb
lssalgado <lucas.salgado@uniriotec.br>
2018-08-07 22:37:17 -0300

    Fixing ident.

--- refurbished_getter.py
+++ refurbished_getter.py
@@ -1,19 +1,19 @@
 import os, urllib, json
 
 def getContent(str):
-  response = urllib.urlopen(str)
-  data = json.loads(response.read())
-  return data
+    response = urllib.urlopen(str)
+    data = json.loads(response.read())
+    return data
 
 def selector(obj, aux):
 
-  print("Selecione a fruta desejada:")
+    print("Selecione a fruta desejada:")
 
-  for i in obj:
-      print str(aux) + " - " + i["name"] + ": $" + str(i["price"])
-      aux = aux + 1
+    for i in obj:
+        print str(aux) + " - " + i["name"] + ": $" + str(i["price"])
+        aux = aux + 1
     
-  return getInput(aux)
+    return getInput(aux)
 
 def getInput(aux):
     while True:
@@ -32,21 +32,20 @@ def getInput(aux):
     return value
 
 def detailer(obj):
-  return obj["name"] + "\nPrice:  $" + str(obj["price"]) + "\nPreco: R$" + str(obj["price"]*3.5)
+    return obj["name"] + "\nPrice:  $" + str(obj["price"]) + "\nPreco: R$" + str(obj["price"]*3.5)
 
 def main():
-  os.system('cls||clear')
-  
-  url = "https://raw.githubusercontent.com/muxidev/desafio-android/master/fruits.json"
-  counter = 0
-  dolarToReal = 3.5
-  fruit = 0
+    os.system('cls||clear')
 
-  fruitsJson = getContent(url)["fruits"]
-  
-  fruit = selector(fruitsJson, counter)
-  
-  print detailer(fruitsJson[fruit])
+    url = "https://raw.githubusercontent.com/muxidev/desafio-android/master/fruits.json"
+    counter = 0
+    dolarToReal = 3.5
+    fruit = 0
+    fruitsJson = getContent(url)["fruits"]
+
+    fruit = selector(fruitsJson, counter)
+
+    print detailer(fruitsJson[fruit])
 
 if __name__ == "__main__":
-  main()
+    main()
